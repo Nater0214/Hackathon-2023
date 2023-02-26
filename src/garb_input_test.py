c45fbc64294e79a4fb60a22b26388267fff6c214
Nater0214 <gamer@nater0214.com>
2023-02-26 13:22:43 -0500

    Update tests

--- src/garb_input_test.py
+++ src/garb_input_test.py
@@ -4,18 +4,24 @@ import src.garb_input as garb_input
 
 def test_mine_1():
     assert garb_input.decipher("ap gov assignMent fEb 14") == {
-        "name": "Ap gov assignment",
+        "name": "Ap Gov Assignment",
         "date": datetime.date(2023, 2, 14)
     }
 
 def test_vibhor_1():
     assert garb_input.decipher("spanish test february 24") == {
-        "name": "Spanish test",
+        "name": "Spanish Test",
         "date": datetime.date(2023, 2, 24)
     }
 
 def test_anish_1():
     assert garb_input.decipher("ap gov test march 2") == {
-        "name": "Ap gov test",
+        "name": "Ap Gov Test",
         "date": datetime.date(2023, 3, 2)
+    }
+
+def test_vibhor_2():
+    assert garb_input.decipher("submit application tomorrow") == {
+        "name": "Submit Application",
+        "date": datetime.date(2023, 2, 27)
     }
