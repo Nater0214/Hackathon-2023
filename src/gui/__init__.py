82d846ab02b9a7f1105f3cad44f6a075d7a02d2d
Nater0214 <gamer@nater0214.com>
2023-02-26 14:37:05 -0500

    Add setup window

--- src/gui/__init__.py
+++ src/gui/__init__.py
@@ -7,7 +7,7 @@ from PySide6 import QtCore as Qt
 from PySide6.QtGui import QStandardItem, QStandardItemModel
 from PySide6.QtWidgets import QApplication, QMainWindow
 
-from . import ui_start
+from . import ui_start, ui_setup
 
 
 # Definitions
@@ -21,14 +21,34 @@ class StartWindow(QMainWindow, ui_start.Ui_MainWindow):
         self.setupUi(self)
         
         self.app = app
+        
+        # Button connections
+        self.setupButton.clicked.connect(self.setup_ui_start)
     
     
     def closeEvent(self, event) -> None:
         self.app.quit()
+    
+    
+    # Window changers
+    def setup_ui_start(self) -> None:
+        """Open the setup UI"""
+        
+        setup_window.show()
+
+
+class SetupWindow(QMainWindow, ui_setup.Ui_MainWindow):
+    def __init__(self):
+        # Init
+        
+        super(SetupWindow, self).__init__()
+        self.setupUi(self)
 
 
 # Create the windows
 def create_windows(app: QApplication) -> None:
-    global start_window 
+    global start_window
+    global setup_window
     
     start_window = StartWindow(app)
+    setup_window = SetupWindow()
