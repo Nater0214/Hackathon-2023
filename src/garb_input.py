e088f955ed83fb4115b27a7f92966d34c9538f3a
Nater0214 <gamer@nater0214.com>
2023-02-26 12:48:47 -0500

    Get first iteration of function working

--- src/garb_input.py
+++ src/garb_input.py
@@ -3,7 +3,7 @@
 
 
 # Imports
-import calendar
+import datetime
 
 
 # Definitions
@@ -12,26 +12,48 @@ def decipher(txt: str) -> dict:
     
     # Convert input to all lower
     txt = txt.lower()
+    txt = txt.split(' ')
     
     # Find date
     
     # Look for a month
     for month in ("january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"):
-        if (month_ind := txt.find(month)) >= 0 or (month_ind := txt.find(month[0:3])) >= 0:
-            break
+        try:
+            if (month_ind := txt.index(month)) >= 0:
+                break
+        except ValueError:
+            pass
+        
+        try:
+            if (month_ind := txt.index(month[:3])) >= 0:
+                break
+        except ValueError:
+            pass
     
     else:
         month = None
     
     # Look for date if there is a month
     if month:        
-        for date in range(1, 32):
-            if (date_ind := txt.find(str(date), month_ind)):
-                break
+        for day in range(1, 32):
+            try:
+                if (date_ind := txt.index(str(day), month_ind)):
+                    break
+            except ValueError:
+                continue
         
         else:
-            date = None
+            day = None
         
-        # Look for year if there is a date
-        if date:
-            
+        
+        if day:
+            date = datetime.datetime.strptime(f"{month} {day} {datetime.date.today().year}", "%B %d %Y")
+            date = datetime.date(date.year, date.month, date.day)
+    
+    name = ' '.join(txt[:month_ind])
+    name = name.capitalize()
+    
+    return {
+        "name": name,
+        "date": date
+    }
